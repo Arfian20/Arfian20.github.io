1528b944be7b97e2a913be77c6ca6dfe24a83204
Arfian20 <arfiansurya20@gmail.com>
2021-04-09 19:10:46 +0700

    menambah jam digital

---
+++ jsdei.js
@@ -0,0 +1,23 @@
+// const container = document.querySelector(".container");
+// container.setAttribute("type", "button");
+// container.addEventListener("mouseenter", function () {
+//   const r = Math.round(Math.random() * 255 + 1);
+//   const g = Math.round(Math.random() * 255 + 1);
+//   const b = Math.round(Math.random() * 255 + 1);
+//   document.body.style.backgroundColor = "rgb(" + r + ", " + g + ", " + b + ")";
+// });
+
+// function updateClock() {
+//   var now = new Date();
+//   var dname = now.getDay(),
+//       mo = now.getMonth(),
+
+//   var months = ["Januari", "Februari", "Maret", "April", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
+//   var week = ["Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"];
+//   var ids = ["day", "month", "dayNum", "year", "hour", "minutes", "second", "period"];
+
+//   function initClock() {
+//     updateClock();
+//     window.setInterval("updateClock()", 1);
+//   }
+// }
